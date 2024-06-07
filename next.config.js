b875ac5c371ff6e76405e6e236f4e1d7a6efe7dd
Gabriel Samson <gabriel@slopepay.com>
2024-06-07 16:33:44 -0700

    cors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'demo.slopepay.com',
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'slopepay.retool.com',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
