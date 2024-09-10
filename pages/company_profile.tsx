28688dfd5ec594b693f0feee6efcc34118342455
Victor Klochikhin <victor@slope.so>
2024-09-10 12:10:18 -0700

    more nav buttons

--- pages/company_profile.tsx
+++ pages/company_profile.tsx
@@ -17,7 +17,7 @@ const Orders = () => {
       <Box pos='relative' w={1400} h={800} sx={{ flexShrink: 0, overflow: 'auto' }}>
         <Image pos='absolute' width='100%' src='/images/alibaba_company_profile.png' alt='Alibaba Company Profile' />
         <Box pos='relative' w='100%' h='100%'>
-          <Button pos='absolute' left={250} top={12} w={104} h={36} sx={{ opacity: 0, ':hover': { opacity: 0.1 } }} onClick={handleClickMyAccount} />
+          <Button pos='absolute' left={250} top={12} w={104} h={36} sx={{ opacity: 0.1, ':hover': { opacity: 0.2 } }} onClick={handleClickMyAccount} />
         </Box>
       </Box>
     </Center>
