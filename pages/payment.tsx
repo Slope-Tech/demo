77688982cf8f51a37c05e41ea4f445f2d4aff5a9
Victor Klochikhin <victor@slope.so>
2024-09-11 14:28:00 -0700

    format

--- pages/payment.tsx
+++ pages/payment.tsx
@@ -1,6 +1,6 @@
-import { Box, Button, Center, Image } from "@mantine/core"
-import { useRouter } from "next/router"
-import { useState } from "react"
+import { Box, Button, Center, Image } from '@mantine/core'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
 
 declare global {
   interface Window {
@@ -10,7 +10,7 @@ declare global {
   }
 }
 
-const Payment = ({ order, secret }: { order: any, secret: any }) => {
+const Payment = ({ order, secret }: { order: any; secret: any }) => {
   const router = useRouter()
 
   const [loading, setLoading] = useState(false)
@@ -49,13 +49,33 @@ const Payment = ({ order, secret }: { order: any, secret: any }) => {
     setLoading(false)
   }
 
-
   return (
-    <Center w='100%' h='100%'>
-      <Box pos='relative' w={1400} h={800} sx={{ flexShrink: 0}}>
-        <Image pos='absolute' width='100%' height='100%' src='/images/alibaba_payment.png' alt='Alibaba Payment' />
-        <Box pos='relative' w='100%' h='100%'>
-          <Button pos='absolute' top={473} right={184} w={284} h={36} radius='xl' color='orange.7' bg='#FF6600' onClick={handleClickSlope} leftIcon={<img alt="Slope Logo" src="/images/icon_white.svg" height={22} />} loading={loading} fz='md' styles={{ icon: { marginLeft: 6 }, label: { flexGrow: 1, justifyContent: 'center' }}} >
+    <Center w="100%" h="100%">
+      <Box pos="relative" w={1400} h={800} sx={{ flexShrink: 0 }}>
+        <Image
+          pos="absolute"
+          width="100%"
+          height="100%"
+          src="/images/alibaba_payment.png"
+          alt="Alibaba Payment"
+        />
+        <Box pos="relative" w="100%" h="100%">
+          <Button
+            pos="absolute"
+            top={473}
+            right={184}
+            w={284}
+            h={36}
+            radius="xl"
+            color="orange.7"
+            bg="#FF6600"
+            onClick={handleClickSlope}
+            leftIcon={<img alt="Slope Logo" src="/images/icon_white.svg" height={22} />}
+            loading={loading}
+            disabled={!order || !secret}
+            fz="md"
+            styles={{ icon: { marginLeft: 6 }, label: { flexGrow: 1, justifyContent: 'center' } }}
+          >
             Pay with Slope
           </Button>
         </Box>
